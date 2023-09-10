7f58dd7db876076175c36953c7d69c45f060bf2c
yash <ycjaware@gmail.com>
2023-09-10 16:08:28 +0530

    Added all files

---
+++ index.php
@@ -0,0 +1,190 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="styles.css">
+
+  <title>Cosmo Junkie</title>
+  <style>
+    .error{
+      color: red;
+    }
+    #successMessage
+    {
+           
+            text-align: center;
+            height: 5vh;
+            width: 100%;
+            background-color: green; 
+            margin-top: 20px;
+    }
+   
+  </style>
+</head>
+<body>
+  <nav class="site-nav grid">
+    <h1>Cosmo Junkie</h1>
+    <ul>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#skills">Skills</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+  
+  <div id="successMessage" style="display:none;">
+    <span>Message sent successfully!</span>
+</div>
+<section id="welcome" class="grid">
+    <div class="welcome-text">
+      <h2>Space Enthusiast<br />& JavaScript Developer</h2>
+      <p class="leading">Ex NeilsenIQ software Engineer Inter Ex Carer.ai Full stack development Intern</p>
+      <a href="#portfolio" class="button">View my work</a>
+    </div>
+    <!-- toggle class on submit data -->
+
+
+    <div class="welcome-img">
+      <img src="assets/banner_image.png" alt="pic of planet">
+    </div>
+  </section>
+  <section id="portfolio">
+    <h3>Some of my Projects</h3>
+    <div class="projects grid">
+      <a href="#">
+        <img src="assets/project_1.png" alt="space race image">
+        <h4>Space Race Game</h4>
+      </a>
+      <a href="#">
+        <img src="assets/project_2.png" alt="planet boy image">
+        <h4>Planet Boy API</h4>
+      </a>
+      <a href="#">
+        <img src="assets/project_3.png" alt="captain cosmo image">
+        <h4>Captain Cosmo Blog</h4>
+      </a>
+      <!-- <a href="#">
+        <img src="assets/project_3.png" alt="captain cosmo image">
+        <h4>Captain Cosmo Blog 2</h4>
+      </a> -->
+    </div>
+  </section>
+  <section id="skills">
+    <h3>Things I Can Do</h3>
+    <ul class="grid">
+      <li>
+        <img src="assets/comet_1.svg" alt="comet">
+        <h4>JavaScript</h4>
+      </li>
+      <li>
+        <img src="assets/comet_2.svg" alt="comet">
+        <h4>React</h4>
+      </li>
+      <li>
+        <img src="assets/comet_3.svg" alt="comet">
+        <h4>Node</h4>
+      </li>
+      <li>
+        <img src="assets/comet_4.svg" alt="comet">
+        <h4>Express</h4>
+      </li>
+    </ul>
+    <p class="leading">I have done development in Django,Flask,React,Node,Express MongoDB so I have good understanding of Full stack development.</p>
+  </section>
+
+  <section id="contact">
+    <h3>Get In Touch</h3>
+    <p class="leading"> If there is any query free to ask me </p>
+
+    
+    <form id="form" action="" method="post" onsubmit="return validateForm();">
+      <span id="nameError" class="error"></span>
+      <input type="text" placeholder='NAME' name="name" id="name">
+      <span id="emailError" class="error"></span>
+      <input type="email" placeholder='EMAIL' name="email" id="email">
+      <span id="subjectError" class="error"></span>
+      <input type="text" placeholder='SUBJECT' name="subject" id="subject">
+      <span id="messageError" class="error"></span>
+      <textarea placeholder='YOUR MESSAGE' name="messege" id="messege"></textarea>
+    
+      <button class="button" id="btn" type="submit" value="Submit">Send</button>
+    </form>
+  </section>
+ 
+  <footer>
+    <div class="grid">
+      <p class="copyright">Copyright 2023 Cosmo Junkie</p>
+      <ul class="social">
+        <li><a href="#"><img src="assets/icon_fb.svg" alt="facebook"></a></li>
+        <li><a href="#"><img src="assets/icon_tw.svg" alt="twitter"></a></li>
+      </ul>
+    </div>
+  </footer>
+
+  <script src="./script.js"></script>
+  <?php
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['messege']))
+{
+$name=$_POST['name'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$messege=$_POST['messege'];
+ 
+
+// Database connection 
+$conn=new mysqli('localhost','root','','contacts');
+if($conn->connect_error){
+    die('Connecttion failed : '.$conn->connect_error);
+}
+else
+{
+    // Validate name
+    if (empty($name)) 
+    {
+        $nameErr = "Name is required";
+    } 
+
+    // Validate email
+    if (empty($email)) 
+    {
+        $emailErr = "Email is required";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+    {
+        $emailErr = "Invalid email format";
+    }
+    if (empty($subject)) 
+    {
+        $subjectErr = "Subject is required";
+    }
+
+    // Validate message
+    if (empty($messege)) 
+    {
+        $messageErr = "Message is required";
+    }
+   
+// echo empty($nameErr),empty($emailErr),empty($messageErr);
+    if (empty($nameErr) && empty($emailErr) && empty($messageErr) && empty($subjectErr)) 
+    {
+       
+    $stmt=$conn->prepare("insert into messeges(name,email,subject,messege)values(?,?,?,?)");
+    $stmt->bind_param("ssss",$name,$email,$subject,$messege);
+    $stmt->execute();
+    // echo "Thanks for the connecting";
+    $stmt->close();
+    $conn->close();
+   
+    }
+     else
+     {
+//  echo 'Plese fill form properly',$nameErr,$emailErr,$messageErr,$subjectErr;
+     }
+
+}
+}
+?>
+
+  
+</body>
+</html>
+
---
+++ script.js
@@ -0,0 +1,116 @@
+// Function to validate the contact form
+const form = document.getElementById('form');
+
+function validateForm() 
+{
+    
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const subject = document.getElementById('subject').value.trim();
+    const message = document.getElementById('messege').value.trim();
+    let isValid = true;
+
+    // Reset error messages
+    document.getElementById('nameError').textContent = '';
+    document.getElementById('emailError').textContent = '';
+    document.getElementById('subjectError').textContent = '';
+    document.getElementById('messageError').textContent = '';
+
+    // Rest of your validation code remains the same
+    // ...
+    // Validate name
+    if (name === '') 
+    {
+        document.getElementById('nameError').textContent = 'Name is required';
+        console.log('name is required');
+        isValid = false;
+    }
+
+    // Validate email
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (email === '' || !emailPattern.test(email)) {
+        document.getElementById('emailError').textContent = 'Invalid email format';
+        isValid = false;
+    }
+
+    // Validate subject
+    if (subject === '') {
+        document.getElementById('subjectError').textContent = 'Subject is required';
+        isValid = false;
+    }
+
+    // Validate message
+    if (message === '') 
+    {
+        document.getElementById('messageError').textContent = 'Message is required';
+        isValid = false;
+    }
+
+    // Prevent form submission if there are validation errors
+    if (isValid) 
+    {
+        // Show the success message
+       
+        document.getElementById('successMessage').style.display = 'block';
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Smooth scrolling to the top
+        console.log('sucess ');
+        // Automatically hide the success message after 3 seconds (adjust the time as needed)
+        setTimeout(function () 
+        {
+            document.getElementById('successMessage').style.display = 'none';
+            
+        }, 2000); // 2000 milliseconds (2 seconds)
+
+        
+
+        return true; // Allow the form to be submitted
+    } else 
+    {
+        return false; // Prevent the form from being submitted
+    }
+    
+}
+
+    //for mobile devices
+let btn=document.getElementById('btn');
+btn.addEventListener('ontouchstart',(e)=>
+{
+    if(validateForm())
+    {
+        
+        document.getElementById('successMessage').style.display = 'block';
+         window.scrollTo({ top: 0, behavior: 'smooth' });
+        setTimeout(function () 
+        {
+            document.getElementById('successMessage').style.display = 'none';
+            
+        }, 5000); // 5000 milliseconds (5 seconds)
+        form.dispatchEvent(new Event('submit'));
+
+    }
+}) 
+
+ form.addEventListener('submit',(e)=>
+{
+    e.preventDefault();   
+    
+    if(validateForm())
+    {
+        
+    setTimeout( () => {
+        form.submit()
+        const name = document.getElementById('name');
+        name.value='';
+        const email = document.getElementById('email');
+        email.value='';
+        const subject = document.getElementById('subject');
+        subject.value='';
+        const message = document.getElementById('messege');
+        message.value='';
+    
+    }, 4000);
+    console.log('Submitting');
+    }
+    
+})
+
